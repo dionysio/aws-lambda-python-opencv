c1377d69c719e96e21649f98f7dae7fd34b11688
Dio <contact@dionysio.com>
2018-08-22 16:35:46 +0200

    updated build script to opencv 3.4

--- build.sh
+++ build.sh
@@ -2,20 +2,20 @@
 
 # Setting up build env
 sudo yum update -y
-sudo yum install -y git cmake gcc-c++ gcc python-devel chrpath
+sudo yum install -y git cmake gcc-c++ gcc python36 python36-devel chrpath
 mkdir -p lambda-package/cv2 build/numpy
 
 # Build numpy
-pip install --install-option="--prefix=$PWD/build/numpy" numpy
-cp -rf build/numpy/lib64/python2.7/site-packages/numpy lambda-package
+pip-3.6 install --install-option="--prefix=$PWD/build/numpy" numpy
+cp -rf build/numpy/lib64/python3.6/site-packages/numpy lambda-package
 
-# Build OpenCV 3.2
+# Build OpenCV 3.4
 (
 	NUMPY=$PWD/lambda-package/numpy/core/include
 	cd build
 	git clone https://github.com/Itseez/opencv.git
 	cd opencv
-	git checkout 3.2.0
+	git checkout 3.4.0
 	cmake										\
 		-D CMAKE_BUILD_TYPE=RELEASE				\
 		-D WITH_TBB=ON							\
@@ -30,17 +30,18 @@ cp -rf build/numpy/lib64/python2.7/site-packages/numpy lambda-package
 		-D BUILD_EXAMPLES=OFF					\
 		-D BUILD_TESTS=OFF						\
 		-D BUILD_PERF_TESTS=OFF					\
-		-D PYTHON2_NUMPY_INCLUDE_DIRS="$NUMPY"	\
+		-D PYTHON_DEFAULT_EXECUTABLE=/usr/bin/python36			\
+		-D PYTHON3_NUMPY_INCLUDE_DIRS="$NUMPY"	\
 		.
 	make -j`cat /proc/cpuinfo | grep MHz | wc -l`
 )
 cp build/opencv/lib/cv2.so lambda-package/cv2/__init__.so
-cp -L build/opencv/lib/*.so.3.2 lambda-package/cv2
+cp -L build/opencv/lib/*.so.3.4 lambda-package/cv2
 strip --strip-all lambda-package/cv2/*
 chrpath -r '$ORIGIN' lambda-package/cv2/__init__.so
 touch lambda-package/cv2/__init__.py
 
 # Copy template function and zip package
-cp template.py lambda-package/lambda_function.py
+cp lambda_function.py lambda-package/lambda_function.py
 cd lambda-package
 zip -r ../lambda-package.zip *
